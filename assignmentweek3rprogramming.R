749b1df28399323c9b25de77acbe94696846dd36
Nicholas Bodanac <nbodanac@gmail.com>
2019-03-19 10:50:52 +0300

    assignment week 3 for r programming in coursera

---
+++ assignmentweek3rprogramming.R
@@ -0,0 +1,29 @@
+# this will run the inverse of the below  matrix called makeCacheMatrix.
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+# If the inverse has been calculated the cacheSolve will retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
